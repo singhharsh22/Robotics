da7d810c473851f5aa9ac7c7bfeff5c3088aa692
Harsh Singh <115803634+singhharsh22@users.noreply.github.com>
2025-08-31 18:05:40 +0530

    Add files via upload

---
+++ Exp2_Arduino_basics_2/Task6_serial_transmitter_and_receiver/MicrocontrollerB_serial_receiver/MicrocontrollerB_serial_receiver.ino
@@ -0,0 +1,27 @@
+// ME381 ROBOTICS: Experiment 2
+// Task6: UART serial receiver Board B
+// Upload this code to Board B. Enter a number in the range 0-10 from the serial monitor and observe the LED of this board
+
+const int ledPin = 13;  // LED pin
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  // Set the LED pin as an output
+  Serial.begin(9600);       // Initialize serial communication at 9600 bps
+}
+
+void loop() {
+  if (Serial.available() > 0) {   // Check if data is available to read
+    int num = Serial.parseInt();  // Read the number
+    blinkLED(num);                // Blink the LED the number of times received
+  }
+}
+
+void blinkLED(int num) {
+  int interval = 5000 / num;  // Calculate the interval for blinking within 5 seconds
+  for (int i = 0; i < num; i++) {
+    digitalWrite(ledPin, HIGH);  // Turn the LED on
+    delay(interval / 2);         // Wait for half of the interval
+    digitalWrite(ledPin, LOW);   // Turn the LED off
+    delay(interval / 2);         // Wait for the other half of the interval
+  }
+}
